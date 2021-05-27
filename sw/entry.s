5fb781d9307c92c833ba81b3f7344b82a2c9160e
Noah Moroze <noah@zeroasic.com>
2021-05-27 18:32:02 -0400

    Clean up linking
    Rewrite linker file based on OpenTitan's boot ROM linker file. This lets
    us statically link libgcc with no issues.

--- sw/entry.s
+++ sw/entry.s
@@ -1,18 +1,19 @@
-.global _entry
+# Exception handler - for now, just loops forever.
+.org 0x00
+_exception_handler:
+  j _exception_handler
+
+# Main code entry point
+.org 0x80
 _entry:
-    # # set up scratch space for saving regs on exception handler
-    # la x1, _mscratch
-    # csrw mscratch, x1
-    # # set up exception handler
-    # la x1, _handler
-    # csrw mtvec, x1
+    # TODO: should do more reset (set up exception-handling stuff, clear all regs)
 
     # set stack pointer
-    la sp, _stack_start
+    la sp, _stack_end
 
     # call main function
     call main
 
 _hang:
-    # hang forever
-    jal x0, _hang
+    # hang if main returns
+    j _hang
