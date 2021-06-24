d0a6ac9733ce88068d471b84d3546e9452fd9e2f
Noah Moroze <noah@zeroasic.com>
2021-06-24 17:24:49 -0400

    Messing with floorplan

--- asic/floorplan.py
+++ asic/floorplan.py
@@ -1,15 +1,20 @@
 def setup_floorplan(fp, chip):
-    fp.create_die_area(200, 200)
+    # TODO: would be nice to have die area calculated from Yosys
+    # stuff/utilization
+    # TODO: would be nice to have core area calculated based on pad cell heights
+    fp.create_die_area(200, 200, core_area=())
 
-    pins = ['clk', 'rst', 'uart_rx', 'uart_tx'] + [f'gpio[{i}]' for i in range(32)]
-    N = len(pins) / 4 # pins per side
+    pins_w = ['gpio[{i}]' for i in range(5)] + ['vss1', 'vdd1', 'vddio1'] + ['clk', 'rst', 'uart_rx', 'uart_tx']
+    pins_n = ['gpio[{i}]' for i in range(5, 10)] + ['vss2', 'vdd2', 'vddio2'] + ['gpio[{i}]' for i in range(10, 14)]
+    pins_e = ['gpio[{i}]' for i in range(14, 19)] + ['vss2', 'vdd2', 'vddio2'] + ['gpio[{i}]' for i in range(19, 23)]
+    pins_s = ['gpio[{i}]' for i in range(23, 28)] + ['vss3', 'vdd3', 'vddio3'] + ['gpio[{i}]' for i in range(28, 32)]
 
     metal = 'm3'
     width = 1
     depth = 3
-    fp.place_pins(pins[0:N], 'w', width, depth, metal)
-    fp.place_pins(pins[N:2*N], 'n', width, depth, metal)
-    fp.place_pins(pins[2*N:3*N], 'e', width, depth, metal)
-    fp.place_pins(pins[3*N:], 's', width, depth, metal)
+    fp.place_pins(pins_w, 'w', width, depth, metal)
+    fp.place_pins(pins_n, 'n', width, depth, metal)
+    fp.place_pins(pins_e, 'e', width, depth, metal)
+    fp.place_pins(pins_s, 's', width, depth, metal)
 
     return fp
