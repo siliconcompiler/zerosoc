6023584c0508206d86cf3cbe8172d187f86addbf
Noah Moroze <noah@zeroasic.com>
2021-07-25 16:22:30 +0000

    Add back RAM and pads to all floorplan sides
    This floorplan is routable, takes 8 hours

--- asic/sky130/floorplan.py
+++ asic/sky130/floorplan.py
@@ -27,7 +27,7 @@ def setup_floorplan(fp, chip):
     ram_y = fp.snap(die_h - margin - ram_h - 50 * std_cell_h, std_cell_h)
 
     fp.create_die_area(die_w, die_h,
-        core_area=(margin, margin, die_w - margin, die_h - margin))
+        core_area=(margin, margin, ram_x - ram_core_space, die_h - margin))
 
     # Place RAM
     # Must be placed outside core area to ensure we don't run into routing
@@ -54,26 +54,24 @@ def setup_floorplan(fp, chip):
     # TODO: place pins directly under pad metal area
     die_w, die_h = fp.die_area
 
-    # # west
-    # pads_width = sum(fp.available_cells[cell].width for _, cell in pads_w)
-    # spacing = (die_h - corner_w - corner_h - pads_width) / (len(pads_w) + 1)
-
-    # y = corner_h + spacing
-    # for pad_name, pad_type in pads_w:
-    #     width = fp.available_cells[pad_type].width
-    #     height = fp.available_cells[pad_type].height
-
-    #     fp.place_macros([(pad_name, pad_type)], 0, y, 0, 0, 'W')
-    #     # fp.place_pin([pin_name], 0, y, 0, 0, 10, 10, 'm5', 'N')
-
-    #     y += width + spacing
-
     pin_depth_offset = 60
     pin_size = 10
 
-    # north
-    pads_n = pads_w + pads_n
+    # west
+    pads_width = sum(fp.available_cells[cell].width for _, _, cell in pads_w)
+    spacing = fp.snap((die_h - corner_w - corner_h - pads_width) / (len(pads_w) + 1), 1)
 
+    y = corner_h + spacing
+    for pad_name, pin_name, pad_type in pads_w:
+        width = fp.available_cells[pad_type].width
+        height = fp.available_cells[pad_type].height
+
+        fp.place_macros([(pad_name, pad_type)], 0, y, 0, 0, 'W')
+        # fp.place_pin([pin_name], 0, y, 0, 0, 10, 10, 'm5', 'N')
+
+        y += width + spacing
+
+    # north
     pads_width = sum(fp.available_cells[cell].width for _, _, cell in pads_n)
     spacing = fp.snap((die_w - corner_w - corner_h - pads_width) / (len(pads_n) + 1), 1)
 
@@ -87,20 +85,19 @@ def setup_floorplan(fp, chip):
 
         x += width + spacing
 
-    # # east
-    # pads_width = sum(fp.available_cells[cell].width for _, cell in pads_e)
-    # spacing = (die_h - corner_w - corner_h - pads_width) / (len(pads_e) + 1)
-    # y = corner_w + spacing
-    # for pad_name, pad_type in pads_e:
-    #     width = fp.available_cells[pad_type].width
-    #     height = fp.available_cells[pad_type].height
+    # east
+    pads_width = sum(fp.available_cells[cell].width for _, _, cell in pads_e)
+    spacing = fp.snap((die_h - corner_w - corner_h - pads_width) / (len(pads_e) + 1), 1)
+    y = corner_w + spacing
+    for pad_name, pin_name, pad_type in pads_e:
+        width = fp.available_cells[pad_type].width
+        height = fp.available_cells[pad_type].height
 
-    #     fp.place_macros([(pad_name, pad_type)], die_w - height, y, 0, 0, 'E')
+        fp.place_macros([(pad_name, pad_type)], die_w - height, y, 0, 0, 'E')
 
-    #     y += width + spacing
+        y += width + spacing
 
     # south
-    pads_s = pads_e + pads_s
     pads_width = sum(fp.available_cells[cell].width for _, _, cell in pads_s)
     spacing = fp.snap((die_w - corner_w - corner_h - pads_width) / (len(pads_s) + 1), 1)
 
