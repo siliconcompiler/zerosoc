bee114a765a1e9fd51b333c9e4d78acc01a0be8e
Peter Gadfort <gadfort@zeroasic.com>
2023-03-01 04:40:04 -0500

    add global connect scripts and power grid

---
+++ openroad/global_connect_top.tcl
@@ -0,0 +1,14 @@
+####################################
+# global connections
+####################################
+add_global_connection -net {vddio} -pin_pattern {VDDIO} -power
+add_global_connection -net {vssio} -pin_pattern {VSSIO} -ground
+
+add_global_connection -net {vdd} -pin_pattern {VDD} -power
+add_global_connection -net {vdd} -pin_pattern {VPWR}
+add_global_connection -net {vdd} -pin_pattern {VPB}
+add_global_connection -net {vdd} -pin_pattern {_vdd}
+add_global_connection -net {vss} -pin_pattern {VSS} -ground
+add_global_connection -net {vss} -pin_pattern {VGND}
+add_global_connection -net {vss} -pin_pattern {VNB}
+add_global_connection -net {vss} -pin_pattern {_vss}
