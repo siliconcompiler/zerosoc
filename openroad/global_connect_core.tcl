8ca6b69b177126baa1b697b690d0b73d82855d95
Peter Gadfort <gadfort@zeroasic.com>
2024-01-04 18:03:23 -0500

    ensure SRAM is connected to power grid

--- openroad/global_connect_core.tcl
+++ openroad/global_connect_core.tcl
@@ -4,8 +4,8 @@
 add_global_connection -net {_vdd} -pin_pattern {VDD} -power
 add_global_connection -net {_vdd} -pin_pattern {VPWR}
 add_global_connection -net {_vdd} -pin_pattern {VPB}
-add_global_connection -net {_vdd} -pin_pattern {vccd1}
+add_global_connection -net {_vdd} -pin_pattern {vdd}
 add_global_connection -net {_vss} -pin_pattern {VSS} -ground
 add_global_connection -net {_vss} -pin_pattern {VGND}
 add_global_connection -net {_vss} -pin_pattern {VNB}
-add_global_connection -net {_vss} -pin_pattern {vssd1}
+add_global_connection -net {_vss} -pin_pattern {gnd}
