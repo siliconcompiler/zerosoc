a87ddc0295bde58012ecf087302497feaba7f205
Peter Gadfort <gadfort@zeroasic.com>
2024-08-27 21:18:26 -0400

    simplify libraries

--- zerosoc_top.py
+++ zerosoc_top.py
@@ -4,8 +4,8 @@ from lambdalib import padring
 import zerosoc_core
 
 
-def setup(chip):
-    lib = Library(chip, 'zerosoc_top', package='zerosoc', auto_enable=True)
+def setup():
+    lib = Library('zerosoc_top', package='zerosoc', auto_enable=True)
     lib.register_source(
         name='zerosoc',
         path=os.path.abspath(os.path.dirname(__file__)))
