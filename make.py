a7b6548ee631ca21dd7233d905ddc2e924bbccfc
Peter Gadfort <gadfort@zeroasic.com>
2025-09-05 11:38:00 -0400

    update to match SC

--- make.py
+++ make.py
@@ -3,47 +3,178 @@
 ZeroSOC build system
 '''
 
-import siliconcompiler
+from siliconcompiler import DesignSchema, Project, ASICProject
+from siliconcompiler.flows import lintflow
+from lambdalib.ramlib import Spram
+from lambdalib.padring import Padring
 
 import argparse
 import os
 import sys
 
-# Libraries
-from lambdapdk.sky130.libs import sky130sram, sky130io
-from siliconcompiler.targets import skywater130_demo, fpgaflow_demo
+# # Libraries
+# from lambdapdk.sky130.libs import sky130sram, sky130io
+from siliconcompiler.targets import skywater130_demo
 
-from siliconcompiler.tools import openroad
-from siliconcompiler.tools._common import get_tool_tasks as _get_tool_tasks
+# from siliconcompiler.tools.openroad import openroad
+# from siliconcompiler.tools._common import get_tool_tasks as _get_tool_tasks
 
-import floorplan as zerosoc_floorplan
-import zerosoc_core
-import zerosoc_top
+# import floorplan as zerosoc_floorplan
+# import zerosoc_core
+# import zerosoc_top
 
 ASIC_CORE_CFG = 'zerosoc_core.pkg.json'
 
 
-def _configure_remote(chip):
-    chip.set('option', 'remote', True)
+from siliconcompiler.flows.asicflow import SV2VASICFlow
+from siliconcompiler.tools.openroad.init_floorplan import InitFloorplanTask
+from siliconcompiler.tools.openroad._apr import OpenROADPSMParameter
 
-    for library in chip.getkeys('library'):
-        if library in ['sky130hd', 'sky130io']:
-            # No need to copy library
-            continue
-        for fileset in chip.getkeys('library', library, 'output'):
-            for filetype in chip.getkeys('library', library, 'output', fileset):
-                # Need to copy library files into build directory for remote run so the
-                # server can access them
-                chip.hash_files('library', library, 'output', fileset, filetype)
-                chip.set('library', library, 'output', fileset, filetype, True, field='copy')
 
-    for tool in chip.getkeys('tool'):
-        for task in chip.getkeys('tool', tool, 'task'):
-            for file_var in chip.getkeys('tool', tool, 'task', task, 'file'):
-                # Need to copy tool files into build directory for remote run so the
-                # server can access them
-                chip.hash_files('tool', tool, 'task', task, 'file', file_var)
-                chip.set('tool', tool, 'task', task, 'file', file_var, True, field='copy')
+
+class OpenTITAN(DesignSchema):
+    def __init__(self):
+        super().__init__("opentitan")
+
+        self.set_dataroot("opentitan",
+                          'git+https://github.com/lowRISC/opentitan.git',
+                          tag='8b9fe4bf2db8ccfac0b26600decf07cf41867e07')
+
+        with self.active_dataroot("opentitan"), self.active_fileset("rtl"):
+            self.add_idir("hw/ip/prim/rtl")
+            self.add_idir("hw/dv/sv/dv_utils")
+
+            # SV packages (need to be added explicitly)
+            self.add_file('hw/top_earlgrey/rtl/top_pkg.sv')
+
+            self.add_file('hw/ip/gpio/rtl/gpio_reg_pkg.sv')
+            self.add_file('hw/ip/gpio/rtl/gpio_reg_top.sv')
+            self.add_file('hw/ip/gpio/rtl/gpio.sv')
+
+            self.add_file('hw/ip/uart/rtl/uart_reg_pkg.sv')
+            self.add_file('hw/ip/uart/rtl/uart_reg_top.sv')
+            self.add_file('hw/ip/uart/rtl/uart_rx.sv')
+            self.add_file('hw/ip/uart/rtl/uart_tx.sv')
+            self.add_file('hw/ip/uart/rtl/uart.sv')
+
+            self.add_file('hw/ip/prim/rtl/prim_alert_pkg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_secded_pkg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_esc_pkg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_otp_pkg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_pad_wrapper_pkg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_ram_1p_pkg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_ram_2p_pkg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_rom_pkg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_subreg_arb.sv')
+            self.add_file('hw/ip/prim/rtl/prim_subreg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_subreg_ext.sv')
+            self.add_file('hw/ip/prim/rtl/prim_subreg_shadow.sv')
+            self.add_file('hw/ip/prim/rtl/prim_intr_hw.sv')
+            self.add_file('hw/ip/prim/rtl/prim_fifo_sync.sv')
+            self.add_file('hw/ip/prim/rtl/prim_filter_ctr.sv')
+            self.add_file('hw/ip/prim/rtl/prim_ram_1p_adv.sv')
+            self.add_file('hw/ip/prim/rtl/prim_secded_64_57_dec.sv')
+            self.add_file('hw/ip/prim/rtl/prim_secded_64_57_enc.sv')
+            self.add_file('hw/ip/prim/rtl/prim_arbiter_ppc.sv')
+            self.add_file('hw/ip/prim/rtl/prim_lc_sync.sv')
+            self.add_file('hw/ip/prim/rtl/prim_esc_receiver.sv')
+            self.add_file('hw/ip/prim/rtl/prim_diff_decode.sv')
+
+            self.add_file('hw/ip/prim_generic/rtl/prim_generic_flop_2sync.sv')
+            self.add_file('hw/ip/prim_generic/rtl/prim_generic_flop.sv')
+            self.add_file('hw/ip/prim_generic/rtl/prim_generic_buf.sv')
+
+            self.add_file('hw/ip/tlul/rtl/tlul_pkg.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_adapter_sram.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_socket_1n.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_socket_m1.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_cmd_intg_chk.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_rsp_intg_gen.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_adapter_reg.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_fifo_sync.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_err.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_err_resp.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_adapter_host.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_cmd_intg_gen.sv')
+            self.add_file('hw/ip/tlul/rtl/tlul_rsp_intg_chk.sv')
+
+            self.add_file('hw/ip/prim/rtl/prim_util_pkg.sv')
+            self.add_file('hw/ip/prim/rtl/prim_secded_pkg.sv')
+
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_pkg.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_top.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_core.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_cs_registers.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_register_file_latch.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_wb_stage.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_load_store_unit.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_ex_block.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_id_stage.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_if_stage.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_prefetch_buffer.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_fetch_fifo.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_csr.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_counter.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_controller.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_decoder.sv')
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_alu.sv')
+
+            self.add_file('hw/ip/lc_ctrl/rtl/lc_ctrl_state_pkg.sv')
+            self.add_file('hw/ip/lc_ctrl/rtl/lc_ctrl_pkg.sv')
+
+            self.add_file("hw/ip/rv_core_ibex/rtl/rv_core_ibex.sv")
+
+            # Hack to work around Yosys + Surelog issue. Even though this is found in
+            # one of our ydirs, we get different synthesis results if this isn't ordered
+            # earlier.
+            self.add_file('hw/vendor/lowrisc_ibex/rtl/ibex_compressed_decoder.sv')
+
+
+class ZeroSOC(DesignSchema):
+    def __init__(self):
+        super().__init__("zerosoc")
+
+        self.set_dataroot("zerosoc", __file__)
+
+        with self.active_dataroot("zerosoc"), self.active_fileset("rtl.core"):
+            self.set_topmodule("asic_core")
+            self.add_idir("hw")
+            self.add_file("hw/xbar_pkg.sv")
+            self.add_file("hw/prim/prim_pkg.sv")
+            self.add_file("hw/uart_core.sv")
+            self.add_file("hw/zerosoc.sv")
+            self.add_file("hw/xbar.sv")
+            self.add_file("hw/tl_dbg.sv")
+            self.add_file("hw/asic_core.v")
+            self.add_file("hw/xbar.sv")
+            self.add_file("hw/xbar.sv")
+            self.add_file('hw/prim/lambdalib/prim_lambdalib_ram_1p.v')
+            self.add_file('hw/prim/lambdalib/prim_lambdalib_clock_gating.v')
+            self.add_file('hw/prim/prim_flop_2sync.sv')
+            self.add_file('hw/prim/prim_ram_1p.sv')
+            self.add_file('hw/prim/prim_flop.sv')
+            self.add_file('hw/prim/prim_clock_gating.sv')
+            self.add_file('hw/prim/prim_buf.sv')
+
+            self.add_define('PRIM_DEFAULT_IMPL=prim_pkg::ImplLambdalib')
+            self.add_define('RAM_DEPTH=512')
+            self.add_define(f'MEM_ROOT={self.get_dataroot("zerosoc")}')
+            self.add_define('SYNTHESIS')
+
+            self.add_depfileset(Spram(), "rtl")
+            self.add_depfileset(OpenTITAN(), "rtl")
+
+        with self.active_dataroot("zerosoc"), self.active_fileset("rtl.top"):
+            self.set_topmodule("asic_top")
+            self.add_file("hw/asic_top.v")
+            self.add_depfileset(self, "rtl.core")
+            self.add_depfileset(Padring(), "rtl")
+
+        with self.active_dataroot("zerosoc"), self.active_fileset("padring.sky130"):
+            self.add_file("openroad/padring.tcl", filetype="tcl")
+
+        with self.active_dataroot("zerosoc"), self.active_fileset("sdc.top.sky130"):
+            self.add_file("hw/asic_top.sdc")
 
 
 def build_fpga():
@@ -74,8 +205,6 @@ def _setup_core():
 
     chip.use(sky130sram)
     chip.swap_library('lambdalib_ramlib', 'lambdalib_sky130sram')
-
-    chip.set('asic', 'macrolib', ['sky130_sram_1rw1r_64x256_8'])
 
     # Ignore cells in these libraries during DRC, they violate the rules but are
     # foundry-validated
@@ -236,17 +365,36 @@ def _setup_top_hier(core_chip):
     return chip
 
 
-def build_top_flat(verify=True, resume=False, remote=False, floorplan=False):
-    chip = _setup_top_flat()
-    chip.set('option', 'clean', not resume)
+def build_top_flat(resume=True, remote=False, floorplan=False):
+    project = ASICProject(ZeroSOC())
+    project.add_fileset("rtl.top")
+    project.add_fileset("sdc.top.sky130")
 
-    chip.set('option', 'breakpoint', floorplan and not remote, step='floorplan')
+    project.load_target(skywater130_demo.setup)
+    project.set_flow(SV2VASICFlow())
+
+    init_fp: InitFloorplanTask = project.get_task(filter=InitFloorplanTask)
+    init_fp.add_openroad_padringfileset("padring.sky130")
+
+    for task in project.get_task(filter=OpenROADPSMParameter):
+        task.add("var", "psm_skip_nets", 'ioring*')
+        task.add("var", "psm_skip_nets", 'v*io')
+
+    project.get_areaconstraints().set_diearea_rectangle(1700, 2300, coremargin=230)
+    # chip = _setup_top_flat()
+
+    project.set('option', 'clean', not resume)
+
+    project.set('option', 'breakpoint', floorplan and not remote, step='floorplan')
+
+    project.run()
+    project.summary()
 
     _run_build(chip, remote)
     if verify:
         _run_signoff(chip, 'write.views', 'write.gds', remote)
 
-    return chip
+    return project
 
 
 def build_top(core_chip=None, verify=True, resume=False, remote=False, floorplan=False):
@@ -354,4 +502,12 @@ def _main():
 
 
 if __name__ == '__main__':
-    _main()
+    project = Project(ZeroSOC())
+    project.add_fileset("rtl.top")
+    project.set_flow(lintflow.LintFlow())
+
+    assert project.check_filepaths()
+    project.run()
+
+
+    # _main()
--- openroad/padring.tcl
+++ openroad/padring.tcl
@@ -3,32 +3,52 @@ make_fake_io_site -name IO_SITE -width 1 -height 200
 make_fake_io_site -name IO_CSITE -width 200 -height 204
 
 # Create IO Rows
-make_io_sites -horizontal_site IO_SITE -vertical_site IO_SITE -corner_site IO_CSITE -offset 10 -rotation R180
+make_io_sites -horizontal_site IO_SITE -vertical_site IO_SITE -corner_site IO_CSITE -offset 10 \
+    -rotation_horizontal R180 \
+    -rotation_vertical R180 \
+    -rotation_corner R180 \
 
 # Place pads
-proc place_padring_edge { row dim edge } {
-    upvar sc_cfg  sc_cfg
-    upvar sc_tool sc_tool
-    upvar sc_task sc_task
-    set span [ord::dbu_to_microns [[[pad::get_row $row] getBBox] d$dim]]
-    set span_start [ord::dbu_to_microns [[[pad::get_row $row] getBBox] ${dim}Min]]
-    set pad_length [llength [dict get $sc_cfg tool $sc_tool task $sc_task {var} padring_${edge}_name]]
-    set pad_interval [expr $span / ($pad_length + 1)]
-    for { set i 0 } { $i < $pad_length } { incr i } {
-        set pad_cell [get_cells {*}[lindex [dict get $sc_cfg tool $sc_tool task $sc_task {var} padring_${edge}_name] $i]]
-        set pad_name [[sta::sta_to_db_inst $pad_cell] getName]
-        puts "Placing IO pad: $pad_name"
-        place_pad \
-            -row $row \
-            -location [expr $span_start + ($i + 0.5) * $pad_interval] \
-            $pad_name
+proc soc_get_pad_cells {side} {
+    set cells [get_cells -quiet */i${side}/ipad[*].*]
+
+    set insts []
+    set i 0
+    while { [llength $insts] != [llength $cells] } {
+        foreach type "gpio iovdd* iovss*" {
+            set cell [get_cells -quiet */i${side}/ipad[$i].*/${type}]
+            if {[llength $cell] != 0} {
+                break
+            }
+        }
+        incr i
+        if { $i > 100 } {
+            break
+        }
+        if {[llength $cell] == 0} {
+            continue
+        }
+        lappend insts [[sta::sta_to_db_inst $cell] getName]
     }
+
+    return $insts
 }
 
-place_padring_edge IO_WEST y west
-place_padring_edge IO_NORTH x north
-place_padring_edge IO_EAST y east
-place_padring_edge IO_SOUTH x south
+# Handle north
+puts "Place north edge"
+place_pads -row IO_NORTH [soc_get_pad_cells north]
+
+# Handle south
+puts "Place south edge"
+place_pads -row IO_SOUTH [soc_get_pad_cells south]
+
+# Handle west
+puts "Place west edge"
+place_pads -row IO_WEST [lreverse [soc_get_pad_cells west]]
+
+# Handle east
+puts "Place east edge"
+place_pads -row IO_EAST [lreverse [soc_get_pad_cells east]]
 
 # Place corners
 place_corners sky130_ef_io__corner_pad
@@ -44,5 +64,5 @@ place_io_fill -row IO_EAST {*}$iofill
 connect_by_abutment
 
 # Add bond pads
-place_bondpad -bond sky130_ef_io__bare_pad padring.*.i0.gpio -offset "12.5 115"
-place_bondpad -bond sky130_ef_io__bare_pad padring.*.i0.io* -offset "8 95"
+place_bondpad -bond sky130_ef_io__bare_pad padring/*.i0/gpio -offset "12.5 115"
+place_bondpad -bond sky130_ef_io__bare_pad padring/*.i0/io* -offset "8 95"
